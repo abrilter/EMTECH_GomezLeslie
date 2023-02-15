d778c11fd399761d45470ec45c1fde13394b3234
Leslie Gómez <leslie.gomezm@alumno.buap.mx>
2023-02-14 21:52:54 -0600

    semana 5

---
+++ Semana5/variables/variables.js
@@ -0,0 +1,38 @@
+//String
+let nombre = "Panque";
+console.log(nombre);
+
+//Number
+let edad = 21;
+console.log(edad);
+
+//Boolean
+let verdadero = true;
+let falso = false;
+console.log(verdadero);
+console.log(falso);
+
+//Null
+let nulo = null;
+console.log(nulo);
+
+//Undefined
+let undefined = undefined;
+console.log(undefined);
+
+//Concatenar
+console.log('Hola mundo, mi nombre es'+nombre+'y tengo'+edad+'años de edad');
+
+//Interpolación de texto
+console.log(`Hola mundo, mi nombre es ${nombre} y tengo ${edad} años de edad`);
+
+let x = 1;
+let y = 2;
+
+console.log(y-x);
+console.log(x+y);
+
+console.log(x=4);
+console.log(x+=y);
+
+console.log(x==2)
